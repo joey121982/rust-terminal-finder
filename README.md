b10ed071bee2ea93bcead51dd32d4c01d694d35f
joey121982 <bratescuandrei17@gmail.com>
2024-01-15 14:40:07 +0200

    0.2.0 rewrite

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rust-terminal-finder
-command line program that finds a specific file/folder and displays its location. written in rust.
+
+## Version 0.2.0
+
+Command line program that finds a specific file/folder and displays its location. written in rust.
+
+### Usage:
+* rust-terminal-finder 
+    --ftype (or -f) \<fi / fo> 
+    ^ where ```fi``` is for file type and ```fo``` is for folder type.
+    --name (or -n) \<NAME> 
+    --location (or -l) \<LOCATION> (OPTIONAL, if not specified, goes to default)
+    ^ current default is ```/home```
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,45 @@
-use std::{env, fs};
-use std::error::Error;
-use std::process::exit;
-use dirs::home_dir;
+use clap::Parser;
+use std::fs;
 
-fn parse_args(args: Vec<String>, target: &mut String, location: &mut String, extra_args: &mut Vec<String>) {
-    if args.len() < 2 || args[1] == "-help" {
-        println!(
-            "\n\nSimple command line script that finds specified file/folder and prints its location.\n\n\n\
-            USAGE: rust-terminal-finder.exe {{target}} {{start_location}} ...-extra_args\n\n"
-        );
-        exit(0);
-    }
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Type of either File(fi) or Folder(fo)
+    #[arg(short, long)]
+    ftype: String,
 
-    for (index, arg) in args.iter().enumerate() {
-        if index == 0 {
-            continue;
-        }
-        if (arg.chars().collect::<Vec<char>>())[0] != '-' {
-            if target == "@NOT_SPECIFIED" {
-                *target = arg.to_string();
-            } else {
-                *location = arg.to_string();
-            }
-        } else {
-            extra_args.push(arg.to_string());
-        }
-    }
-}
+    /// File/Folder to search for
+    #[arg(short, long)]
+    name: String,
 
-fn search(target: &str, location: &str, args: Vec<String>, mut count: i32) -> Result<(), Box<dyn Error>> {
-    let mut maxcount: i32 = 100;
-
-    for arg in args.clone() {
-        let narg:Vec<char> = arg.chars().collect();
-        if narg[1] == 'm' && narg[2] == 'a' && narg[3] == 'x' {
-            if narg.len() > 5 {
-                maxcount = narg[4].to_digit(10).unwrap() as i32 * 10 + narg[5].to_digit(10).unwrap() as i32
-            } else {
-                maxcount = narg[4].to_digit(10).unwrap() as i32;
-            }
-        }
-    }
-
-    let entries = fs::read_dir(location)?;
-
-    for entry in entries {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_file() {
-            if let Some(file_name) = path.file_name() {
-                if file_name == target {
-                    count += 1;
-                    println!("Found file: {}", path.to_string_lossy());
-                }
-            }
-        } else if path.is_dir() {
-            if let Some(file_name) = path.file_name() {
-                if file_name == target {
-                    count += 1;
-                    println!("Found directory: {}", path.to_string_lossy());
-                }
-            }
-            if count < maxcount {
-                search(target, &path.to_string_lossy(), args.clone(), count)?;
-            }
-        }
-    }
-
-    Ok(())
+    /// Starting location from where to search
+    #[arg(short, long)]
+    location: Option<String>
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let argc: usize = args.len();
-    let mut target: String = String::from("@NOT_SPECIFIED");
-    let mut extra_args: Vec<String> = vec![];
-    let mut location: String = "".to_string();
+    let defaultlocation: String = "/home".to_string();
+    let args = Args::parse();
+    let location: &str = &args.location.unwrap_or(defaultlocation);
+    search(&args.name, &args.ftype, location);
+}
 
-    if let Some(home) = home_dir() {
-        location = home.to_str().unwrap().to_string();
-    }
+fn search(target: &str, ftype: &str, location: &str) {
+    let entries = fs::read_dir(location).expect("ERROR: Location does not exist or permission not granted.");
 
-    parse_args(args, &mut target, &mut location, &mut extra_args);
-
-    if argc == 1 || target == "@NOT_SPECIFIED" {
-        println!("\nIncorrect usage, see -help for help.");
-    } else if argc == 2 {
-        println!("\nLooking for \"{}\", starting at default location: \"{}\"...", target, location);
-    } else if argc == 3 {
-        println!("\nLooking for \"{}\", starting at \"{}\"...", target, location);
-    } else if argc > 3 {
-        print!("\nLooking for \"{}\", starting at \"{}\", with extra args: ", target, location);
-        for arg in extra_args.clone() {
-            print!("{} ", arg);
+    for entry in entries {
+        let path = entry.unwrap().path();
+        if ftype == "fi" && path.is_dir() {
+            continue;
         }
-        println!("...");
-    }
-    println!();
-    if let Err(err) = search(&target, &location, extra_args, 0) {
-        eprintln!("Error: {}", err);
-    } else {
-        println!("\nFinished search. Exiting...\n\n");
+        if ftype == "fo" && path.is_file() {
+            continue;
+        }
+        if path.file_name().unwrap() == target {
+            println!("Found instance at: {}", location);
+        }
+        if ftype == "fo" && path.is_dir() {
+            search(target, ftype, path.to_str().unwrap());
+        }
     }
 }
